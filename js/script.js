de511e9bbefbf821c1c534883e04610f998cf286
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2022-04-14 04:59:38 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,7 +49,9 @@ const dosDaneIcon1104 = L.divIcon({
   iconUrl: "../img/speed-bump.svg",
   iconSize: [40, 40],
   iconAnchor: [20, 20],
-  html:'Hello'
+  html:'<svg width="40" height="40">
+  <circle cx="20" cy="20" r="40" stroke="green" stroke-width="4" fill="yellow" />
+</svg>'
 });
 
 const dosDaneIcon1105 = L.icon({
