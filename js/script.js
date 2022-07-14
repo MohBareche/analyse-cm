0d76c21d5d0df96ddc852344e02b30ace237a250
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2022-07-13 22:42:47 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -268,7 +268,6 @@ const C1106_Layer = L.geoJson(C1106, {
 					<tr>
 						<th># Dos-d'âne</th>
 						<td>${feature.properties.Numero}</td>
-						
 					</tr>
                                         <tr>
 						<th>Devis</th>
@@ -776,38 +775,28 @@ const baseTree = {
 };
 
 var overlaysTree = {
-    label: 'Points of Interest',
+    label: 'Couches de travail',
     selectAllCheckbox: 'Un/select all',
     children: [
         {
-            label: 'Europe',
+            label: 'Projets',
             selectAllCheckbox: true,
             children: [
                 {
-                    label: 'France',
+                    label: '2022',
                     selectAllCheckbox: true,
                     children: [
-                        { label: 'Tour Eiffel', layer: L.marker([48.8582441, 2.2944775]) },
-                        { label: 'Notre Dame', layer: L.marker([48.8529540, 2.3498726]) },
-                        { label: 'Louvre', layer: L.marker([48.8605847, 2.3376267]) },
-                    ]
-                }, {
-                    label: 'Germany',
-                    selectAllCheckbox: true,
-                    children: [
-                        { label: 'Branderburger Tor', layer: L.marker([52.5162542, 13.3776805])},
-                        { label: 'Kölner Dom', layer: L.marker([50.9413240, 6.9581201])},
-                    ]
-                }, {label: 'Spain',
-                    selectAllCheckbox: 'De/seleccionar todo',
-                    children: [
-                        { label: 'Palacio Real', layer: L.marker([40.4184145, -3.7137051])},
-                        { label: 'La Alhambra', layer: L.marker([37.1767829, -3.5892795])},
+                        { label: 'Contrat 1093', layer: C1093_Layer },
+                        { label: 'Contrat 1096', layer: C1096_Layer },
+			{ label: 'Contrat 1104', layer: C1104_Layer },
+			{ label: 'Contrat 1105', layer: C1105_Layer },
+			{ label: 'Contrat 1106', layer: C1106_Layer },
+			{ label: 'Contrat 1108', layer: C1108_Layer }
                     ]
                 }
             ]
         }, {
-            label: 'Asia',
+            label: 'Autres',
             selectAllCheckbox: true,
             children: [
                 {
@@ -817,8 +806,6 @@ var overlaysTree = {
                         { label: 'Petra', layer: L.marker([30.3292215, 35.4432464]) },
                         { label: 'Wadi Rum', layer: L.marker([29.6233486, 35.4390656]) }
                     ]
-                }, {
-                /* ... */
                 }
             ]
         }
